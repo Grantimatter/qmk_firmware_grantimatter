47a77bb7912a35656600ab9569c895b5dbc5ba0e
Grant Wiswell <wiswellgrant@gmail.com>
2023-08-15 20:48:48 -0500

    Fixed gaming layer

--- keyboards/keebio/iris/keymaps/grantimatter/keymap.c
+++ keyboards/keebio/iris/keymaps/grantimatter/keymap.c
@@ -6,15 +6,17 @@
 #include QMK_KEYBOARD_H
 
 #define _DVORAK 0
-#define _LOWER 1
-#define _NAV 2
-#define _GAME 3
+#define _GAME 1
+#define _LOWER 2
+#define _NAV 3
 #define _COLMAKDH 4
 
 #define COPY  C(KC_C)
 #define PASTE C(KC_V)
 #define UNDO  C(KC_Z)
 #define REDO  C(KC_Y)
+
+#define TO_COL TO(_COLMAKDH)
 
 // Home row mods for DVORAK
 #define LG_A LGUI_T(KC_A)
@@ -74,6 +76,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                  //└────────┴───────────┴────────┘                 └─────────┴────────────┴───────┘
   ),
 
+  [_GAME] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────────┐
+     QK_GESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  TO(_DVORAK),
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────────┤
+     KC_TAB,    KC_Q,    KC_R,    KC_W,    KC_E,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,   KC_TRNS,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────────┤
+     KC_LSFT,   KC_F,    KC_A,    KC_S,    KC_D,    KC_I,                             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────────┤
+     KC_LCTL,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,  KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
+  //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────────┘
+                                    KC_TRNS,MO(_LOWER), KC_TRNS,                 KC_TRNS, MO(_NAV), KC_TRNS
+                                // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
+  ),
+
   [_LOWER] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      QK_BOOT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,TO(_GAME),
@@ -90,31 +106,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_NAV] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-  TO(_COLMAKDH), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, QK_BOOT,
+      TO_COL, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_TOG,                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, QK_BOOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_TRNS, KC_TRNS, KC_WH_D,  KC_UP,  KC_WH_U, KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  SC_SENT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TRNS, KC_TRNS, KC_MPRV, KC_MPLY, KC_MNXT, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+     RGB_MOD, KC_TRNS, KC_MPRV, KC_MPLY, KC_MNXT, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
-  [_GAME] = LAYOUT(
-  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────────┐
-      KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  TO(_DVORAK),
-  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────────┤
-      KC_ESC,   KC_Q,    KC_R,    KC_W,    KC_E,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,   KC_TRNS,
-  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────────┤
-      KC_TAB,   KC_F,    KC_A,    KC_S,    KC_D,    KC_I,                             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
-  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────────┤
-     KC_LCTL,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,  KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
-  //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───────┬────┴────────┴────────┴────────┘
-                                    KC_TRNS, KC_TRNS, KC_TRNS,                    KC_SPC, KC_TRNS,  KC_TRNS
-                                // └────────┴────────┴────────┘                 └────────┴────────┴────────────┘
-  ),
 
   [_COLMAKDH] = LAYOUT(
    //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬───────────┐
@@ -122,7 +125,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼───────────┤
       KC_TRNS,   KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                               KC_J,    KC_L,    KC_U,    KC_Y,  KC_SCLN,  KC_TRNS,
    //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼───────────┤
-      KC_BSPC,  GUI_A,   ALT_R,   SFT_S,   HCTL_T,    KC_G,                               KC_M,    CTL_N,  SFT_E,   ALT_I,   GUI_O,    KC_QUOT,
+      KC_BSPC,  GUI_A,   ALT_R,   SFT_S,   HCTL_T,   KC_G,                               KC_M,    CTL_N,  SFT_E,   ALT_I,   GUI_O,    KC_QUOT,
    //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼───────────┤
       KC_TRNS,   KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,  KC_TRNS,          KC_TRNS,   KC_K,    KC_H,  KC_COMM,  KC_DOT, KC_SLSH,  KC_TRNS,
    //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴───────────┘
@@ -161,17 +164,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       break;
     case GAME:
       if (record->event.pressed) {
-        layer_on(_GAME);
-      } else {
-        layer_off(_GAME);
+        set_single_persistent_default_layer(_GAME);
       }
       return false;
       break;
     case COLMAKDH:
       if (record->event.pressed) {
-        layer_on(_COLMAKDH);
-      } else {
-        layer_off(_COLMAKDH);
+        set_single_persistent_default_layer(_COLMAKDH);
       }
       return false;
       break;
